75f3970e16af1cce0f5369027e03300ead1335ee
Moe <rezaalipour.mohammad@gmail.com>
2025-03-16 16:47:33 +0100

    Refactor MutationReport to include timeout info and bad JSON response info

--- src/pyllmut/mutation_report.py
+++ src/pyllmut/mutation_report.py
@@ -2,6 +2,8 @@ from typing import List
 
 from .mutant_lib.mutant_info import MutantInfo
 from .mutant_lib.mutant_type import MutantType
+from .mutant_lib.prompt_info import PromptInfo
+from .mutant_lib.response_info import ResponseInfo
 
 
 class MutationReport:
@@ -9,14 +11,23 @@ class MutationReport:
     A report object that encapsulates the results of a mutant generation process.
     """
 
-    def __init__(self, mutant_list: List[MutantInfo]):
+    def __init__(
+            self,
+            mutant_list: List[MutantInfo],
+            timeout_info_list: List[PromptInfo],
+            bad_response_info_list: List[ResponseInfo]
+    ):
         """
         Initializes a MutationReport with a list of generated mutants.
 
         Args:
             mutant_list (List[MutantInfo]): A list of MutantInfo objects representing the generated mutants.
+            timeout_info_list (List[PromptInfo]): A list of PromptInfo objects for mutant generation attempts (for a line) that timed out.
+            bad_response_info_list (List[ResponseInfo]): A list of ResponseInfo objects for mutant generation attempts (for a line) that model response is not parsable (bad JSON objects).
         """
         self._mutant_list = mutant_list
+        self._timeout_info_list = timeout_info_list
+        self._bad_response_info_list = bad_response_info_list
 
     def get_mutant_list(self) -> List[MutantInfo]:
         """
@@ -39,3 +50,21 @@ class MutationReport:
                              if x.get_mutant_type() == MutantType.VALID]
 
         return valid_mutant_list
+
+    def get_timeout_info_list(self) -> List[PromptInfo]:
+        """
+        Retrieves the list of PromptInfo objects for mutant generation attempts (for a line) that timed out.
+
+        Returns:
+            List[PromptInfo]: A list of PromptInfo objects.
+        """
+        return self._timeout_info_list
+
+    def get_bad_response_info_list(self) -> List[ResponseInfo]:
+        """
+        Retrieves the list of ResponseInfo objects for mutant generation attempts (for a line) that model response is not parsable (bad JSON objects).
+
+        Returns:
+            List[ResponseInfo]: A list of ResponseInfo objects.
+        """
+        return self._bad_response_info_list
